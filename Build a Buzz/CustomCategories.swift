3c0b32218d00ca34d8cabec93c51024dbfbecac5
Chike Chiejine <ce.chiejine@gmail.com>
2018-08-23 18:24:09 +0100

    Added "CustomCategories" class.

---
+++ Build a Buzz/CustomCategories.swift
@@ -0,0 +1,16 @@
+//
+//  CustomCategories.swift
+//  Build a Buzz
+//
+//  Created by C E Chiejine on 23/08/2018.
+//  Copyright © 2018 Color & Space LTD. All rights reserved.
+//
+
+import Foundation
+
+
+
+class CustomCategories: NSObject {
+    
+    
+}
