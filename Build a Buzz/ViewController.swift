48fd129172745202c53fd940431104e12631d2e8
Chike Chiejine <ce.chiejine@gmail.com>
2018-08-23 16:23:01 +0100

    Initial Commit

---
+++ Build a Buzz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Build a Buzz
+//
+//  Created by C E Chiejine on 23/08/2018.
+//  Copyright © 2018 Color & Space LTD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
